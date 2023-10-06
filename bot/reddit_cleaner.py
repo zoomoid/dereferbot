369c4ce184d69a2ede4f85ee9431d0fce2f8645f
Alexander Bartolomey <occloxium@gmail.com>
2023-10-06 12:43:01 +0200

    refactor: anonymize reddit user agent
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- bot/reddit_cleaner.py
+++ bot/reddit_cleaner.py
@@ -2,9 +2,11 @@ from urllib.parse import ParseResult, urlunparse
 from bot.common_cleaner import CommonCleaner, HookReturn
 import requests
 
+from . import __version__
+
 def hook_short_reddit_link(query: ParseResult) -> HookReturn:
     source_url = urlunparse(query)
-    r = requests.get(source_url + ".json", headers={"user-agent": "dereferbot.zoomoid.dev (by /u/occloxium)"})
+    r = requests.get(source_url + ".json", headers={"user-agent": f"web:dereferbot:{__version__} (by /u/deleted)"})
 
     short_url = r.json()[0]["data"]["children"][0]["data"]["url_overridden_by_dest"] or source_url
     return short_url, ("reddit.com -> i.redd.it", "Gets the direct link to an image to embed")
